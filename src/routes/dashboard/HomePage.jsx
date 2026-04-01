1d13022238db9dad62289228321610e747fd7b1a
vlldnt <vieilledent.adrien@gmail.com>
2026-04-01 15:14:54 +0200

    feat: add filters section to HomePage component

--- src/routes/dashboard/HomePage.jsx
+++ src/routes/dashboard/HomePage.jsx
@@ -23,6 +23,9 @@ function HomePage() {
 
   return (
     <div className="homepage-container">
+      <div className="homepage-filters">
+        
+      </div>
       <ul className='genres-list'>
         {GENRES_LIST.map((genre) => (
           <Tag key={genre} genre={genre} genres={genres} setGenres={setGenres} />
